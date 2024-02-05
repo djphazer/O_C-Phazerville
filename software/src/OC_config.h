163f8d1eddec95d994f76453dedc733b7969a8d6
Nicholas J. Michalek <djphazer@gmail.com>
2024-02-05 00:32:37 -0500

    Potential EEPROM fix for T4.1
    It would help if I could figure out where the magic numbers are coming
    from, to verify that they're correct. I'm guessing it's a certain amount of
    page storage overhead.

--- software/src/OC_config.h
+++ software/src/OC_config.h
@@ -50,10 +50,14 @@ static constexpr unsigned long SETTINGS_SAVE_TIMEOUT_MS = 1000;
 #define EEPROM_APPDATA_START EEPROM_GLOBALSETTINGS_END
 #define EEPROM_APPDATA_END EEPROMStorage::LENGTH
 
+#ifdef ARDUINO_TEENSY41
+#define EEPROM_APPDATA_BINARY_SIZE (EEPROM_APPDATA_END - EEPROM_APPDATA_START - 128)
+#else
 // This is the available space for all apps' settings (\sa OC_apps.ino)
 #define EEPROM_APPDATA_BINARY_SIZE (EEPROM_APPDATA_END - EEPROM_APPDATA_START - 92)
 // (I'm not entirely sure where 92 comes from... it was originally (1000 - 4) which leaves 92 bytes free
 //  -NJM
+#endif
 
 #define OC_UI_DEBUG
 #define OC_UI_SEPARATE_ISR
