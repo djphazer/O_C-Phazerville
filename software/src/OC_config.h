f302b8cc1819c4a7a17dce8503e665c5f73bf687
Nicholas J. Michalek <djphazer@gmail.com>
2024-02-07 20:40:20 -0500

    T4.1: more EEPROM adjustments
    I actually verified the calibration data sizes

--- software/src/OC_config.h
+++ software/src/OC_config.h
@@ -41,8 +41,8 @@ static constexpr unsigned long SETTINGS_SAVE_TIMEOUT_MS = 1000;
 #define EEPROM_CALIBRATIONDATA_END 128
 #define EEPROM_GLOBALSETTINGS_END 960
 #else
-#define EEPROM_CALIBRATIONDATA_END 256
-#define EEPROM_GLOBALSETTINGS_END 1280
+#define EEPROM_CALIBRATIONDATA_END 224
+#define EEPROM_GLOBALSETTINGS_END 1144
 #endif
 
 #define EEPROM_GLOBALSETTINGS_START EEPROM_CALIBRATIONDATA_END
@@ -51,11 +51,12 @@ static constexpr unsigned long SETTINGS_SAVE_TIMEOUT_MS = 1000;
 #define EEPROM_APPDATA_END EEPROMStorage::LENGTH
 
 #ifdef ARDUINO_TEENSY41
-#define EEPROM_APPDATA_BINARY_SIZE (EEPROM_APPDATA_END - EEPROM_APPDATA_START - 128)
+#define EEPROM_APPDATA_BINARY_SIZE (EEPROM_APPDATA_END - EEPROM_APPDATA_START - 160)
 #else
 // This is the available space for all apps' settings (\sa OC_apps.ino)
 #define EEPROM_APPDATA_BINARY_SIZE (EEPROM_APPDATA_END - EEPROM_APPDATA_START - 92)
-// (I'm not entirely sure where 92 comes from... it was originally (1000 - 4) which leaves 92 bytes free
+// I'm not entirely sure where 92 comes from...
+// it was originally (1000 - 4) which leaves 92 bytes free (1088 total length on T3.2)
 //  -NJM
 #endif
 
