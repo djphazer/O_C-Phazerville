93fe599ec3be8de99081364dfa365e8e2c07975d
Nicholas J. Michalek <djphazer@gmail.com>
2026-05-04 02:48:55 -0400

    Dynamics: use PSRAM

--- software/src/src/Audio/effect_dynamics.h
+++ software/src/src/Audio/effect_dynamics.h
@@ -58,14 +58,19 @@ public:
 		limitdb = MIN_DB;
 	}
 
-	void Acquire() {
-		samplesSquared = new uint32_t[sampleBufferSize];
-		std::fill_n(samplesSquared, sampleBufferSize, 0);
-	}
+  void Acquire() {
+    samplesSquared = (uint32_t*)extmem_malloc(sampleBufferSize * 4); //new uint32_t[sampleBufferSize];
+    if (samplesSquared)
+      std::fill_n(samplesSquared, sampleBufferSize, 0);
+  }
 
-	void Release() {
-		delete[] samplesSquared;
-	}
+  void Release() {
+    if (samplesSquared) {
+      extmem_free(samplesSquared);
+      samplesSquared = nullptr;
+    }
+    //delete[] samplesSquared;
+  }
 
 	//Sets the gate parameters.
 	//threshold is in dbFS
