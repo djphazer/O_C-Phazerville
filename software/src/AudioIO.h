650066ef1e28d5e93f653ff745110cdf95ffb76d
Nicholas J. Michalek <djphazer@gmail.com>
2026-05-09 00:33:23 -0400

    increase AUDIO_MEMORY to 512 blocks
    using block size of 64 samples at 48kHz,
    this is now about 682ms of shared memory
    I HOPE THAT'S ENOUGH (:

--- software/src/AudioIO.h
+++ software/src/AudioIO.h
@@ -4,7 +4,7 @@
 
 namespace OC {
   namespace AudioIO {
-    const int AUDIO_MEMORY = 256;
+    const int AUDIO_MEMORY = 512;
     AudioStream& InputStream(int interface = 0);
     AudioStream& OutputStream();
     void Init();
