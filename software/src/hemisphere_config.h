a01846cc72b71a78d656084bc63c0e9637d3f882
Nicholas J. Michalek <djphazer@gmail.com>
2026-05-25 16:45:49 -0400

    T32-only; separate applet groups with new #ifdef's
    using PEWPEWPEW by default - "Phazer's Choice"
    Grouped loosely by category, so they're not in alphabetical order, don't @ me.

--- software/src/hemisphere_config.h
+++ software/src/hemisphere_config.h
@@ -46,9 +46,7 @@
 #include "applets/DualQuant.h"
 #include "applets/OffsetQuant.h"
 #include "applets/TwoRings.h"
-#if !defined(CUSTOM_BUILD) || defined(PEWPEWPEW)
 #include "applets/DuoTET.h"
-#endif
 #include "applets/EbbAndLfo.h"
 #include "applets/EnigmaJr.h"
 #include "applets/EnsOscKey.h"
@@ -62,9 +60,7 @@
 #include "applets/Logic.h"
 #include "applets/LowerRenz.h"
 #include "applets/Metronome.h"
-#ifdef __IMXRT1062__
 #include "applets/MidiLoop.h"
-#endif
 #include "applets/MultiScale.h"
 #include "applets/Palimpsest.h"
 #include "applets/Pigeons.h"
@@ -102,22 +98,19 @@
 #include "applets/VectorMod.h"
 #include "applets/VectorMorph.h"
 #include "applets/Voltage.h"
-#ifdef PEWPEWPEW
+#ifdef APPLETS_CRAZYTOWN
 #include "applets/WTVCO.h"
 #endif
 #include "applets/Xfader.h"
 #include "applets/hMIDIIn.h"
 #include "applets/hMIDIOut.h"
 #include "applets/Ponglet.h"
-
-#ifdef PEWPEWPEW
 #include "applets/MarkoV.h"
 #include "applets/MarkovPerc.h"
 #include "applets/EuclidO.h"
 #include "applets/TruthCat3.h"
 #include "applets/TruthCat4.h"
 #include "applets/CVSeq.h"
-#endif
 
 template<class A>
 struct DeclareApplet {
@@ -151,119 +144,167 @@ private:
 };
 
 AppletRegistry reg{
-    DeclareApplet<Ponglet>{99, 0x00},
+
+  /* Well, we can't have it all.
+   * So pick a bundle or two.
+   */
+
 #ifdef PEWPEWPEW
-    DeclareApplet<MarkoV>{98, 0x00},
-    DeclareApplet<MarkovPerc>{97, 0x00},
-    DeclareApplet<EuclidO>{96, 0x00},
-    DeclareApplet<CVSeq>{95, 0x00},
-    DeclareApplet<TruthCat3>{94, 0x00},
-    DeclareApplet<TruthCat4>{93, 0x00},
-#endif
-    DeclareApplet<ADSREG>{8, 0x01},
-    DeclareApplet<ADEG>{34, 0x01},
-    DeclareApplet<ASR>{47, 0x09},
-    DeclareApplet<AttenuateOffset>{56, 0x10},
-#ifdef PEWPEWPEW
-    DeclareApplet<Binary>{41, 0x41},
-#endif
-    DeclareApplet<BitBeat>{79, 0x01},
-    DeclareApplet<BootsNCat>{55, 0x80},
-    DeclareApplet<Brancher>{4, 0x14},
+    // Phazer's choice
     DeclareApplet<BugCrack>{51, 0x80},
+    DeclareApplet<DrLoFi>{16, 0x80},
+
     DeclareApplet<Burst>{31, 0x04},
-    DeclareApplet<Button>{65, 0x10},
-    DeclareApplet<Calculate>{12, 0x10},
-    DeclareApplet<Calibr8>{88, 0x10},
-#ifdef PEWPEWPEW
-    DeclareApplet<Carpeggio>{32, 0x0a},
-    DeclareApplet<Chordinator>{64, 0x08},
-#endif
     DeclareApplet<ClockDivider>{6, 0x04},
     DeclareApplet<ClkToGate>{78, 0x04},
-    DeclareApplet<ClockSkip>{28, 0x04},
-    DeclareApplet<Combin8>{82, 0x10},
-    DeclareApplet<Compare>{30, 0x10},
-    DeclareApplet<Cumulus>{5, 0x40},
-    DeclareApplet<CVRecV2>{24, 0x02},
     DeclareApplet<DivSeq>{68, 0x06},
     DeclareApplet<DivSeq10>{80, 0x06},
-    DeclareApplet<DrLoFi>{16, 0x80},
     DeclareApplet<DrumMap>{57, 0x02},
-    DeclareApplet<DualQuant>{9, 0x08},
-#ifdef PEWPEWPEW
-    DeclareApplet<OffsetQuant>{90, 0x08},
-#endif
-#if !defined(CUSTOM_BUILD) || defined(PEWPEWPEW)
-    DeclareApplet<DuoTET>{63, 0x08},
-#endif
-    DeclareApplet<EbbAndLfo>{7, 0x01},
-    DeclareApplet<EnigmaJr>{45, 0x02},
-    DeclareApplet<EnsOscKey>{35, 0x08},
-    DeclareApplet<EnvFollow>{42, 0x11},
-    DeclareApplet<EnvSeq>{91, 0x02},
     DeclareApplet<EuclidX>{15, 0x02},
-#ifdef PEWPEWPEW
-    DeclareApplet<GameOfLife>{22, 0x01},
-#endif
-    DeclareApplet<GateDelay>{29, 0x04},
-    DeclareApplet<GatedVCA>{17, 0x50},
-    DeclareApplet<Logic>{10, 0x44},
-    DeclareApplet<LowerRenz>{21, 0x01},
-    DeclareApplet<Metronome>{50, 0x04},
-#ifdef __IMXRT1062__
-    DeclareApplet<MidiLoop>{81, 0x20},
-#endif
-    DeclareApplet<hMIDIIn>{150, 0x20},
-    DeclareApplet<hMIDIOut>{27, 0x20},
-#ifdef PEWPEWPEW
-    DeclareApplet<MultiScale>{73, 0x08},
-#endif
-    DeclareApplet<Palimpsest>{20, 0x02},
+    DeclareApplet<MarkoV>{98, 0x00},
+    DeclareApplet<MarkovPerc>{97, 0x00},
     DeclareApplet<Pigeons>{71, 0x02},
+    DeclareApplet<Ponglet>{99, 0x00},
     DeclareApplet<PolyDiv>{72, 0x06},
     DeclareApplet<ProbabilityDivider>{59, 0x04},
     DeclareApplet<ProbabilityMelody>{62, 0x04},
+    DeclareApplet<SequenceX>{14, 0x02},
+    DeclareApplet<Shredder>{58, 0x01},
+    DeclareApplet<Strum>{74, 0x08},
+    DeclareApplet<TwoRings>{18, 0x02},
+    DeclareApplet<TB_3PO>{60, 0x02},
+    DeclareApplet<Squanch>{46, 0x08},
+
+    DeclareApplet<ADSREG>{8, 0x01},
+    DeclareApplet<ADEG>{34, 0x01},
+    DeclareApplet<EbbAndLfo>{7, 0x01},
+    DeclareApplet<EnvSeq>{91, 0x02},
     DeclareApplet<Relabi>{89, 0x01},
-    DeclareApplet<ResetClock>{70, 0x14},
     DeclareApplet<RndWalk>{69, 0x01},
-    DeclareApplet<RunglBook>{44, 0x01},
-    DeclareApplet<ScaleDuet>{26, 0x08},
+    DeclareApplet<Stairs>{61, 0x01},
+    DeclareApplet<VectorEG>{52, 0x01},
+    DeclareApplet<VectorLFO>{49, 0x01},
+    DeclareApplet<VectorMorph>{54, 0x01},
+
+    DeclareApplet<AttenuateOffset>{56, 0x10},
+    DeclareApplet<Brancher>{4, 0x14},
+    DeclareApplet<Button>{65, 0x10},
+    DeclareApplet<Calculate>{12, 0x10},
+    DeclareApplet<Cumulus>{5, 0x40},
+    DeclareApplet<EnvFollow>{42, 0x11},
+    DeclareApplet<Metronome>{50, 0x04},
     DeclareApplet<Schmitt>{40, 0x40},
     DeclareApplet<Scope>{23, 0x80},
+    DeclareApplet<Slew>{19, 0x01},
+    DeclareApplet<Voltage>{43, 0x10},
+    DeclareApplet<Xfader>{33, 0x10},
+#else
+#ifdef APPLETS_SEQUENCING
+    DeclareApplet<ASR>{47, 0x09},
+    DeclareApplet<Burst>{31, 0x04},
+    DeclareApplet<Carpeggio>{32, 0x0a},
+    DeclareApplet<ClockDivider>{6, 0x04},
+    DeclareApplet<ClkToGate>{78, 0x04},
+    DeclareApplet<ClockSkip>{28, 0x04},
+    DeclareApplet<CVSeq>{95, 0x00},
+    DeclareApplet<DivSeq>{68, 0x06},
+    DeclareApplet<DivSeq10>{80, 0x06},
+    DeclareApplet<DrumMap>{57, 0x02},
+    DeclareApplet<EuclidO>{96, 0x00},
+    DeclareApplet<EuclidX>{15, 0x02},
+    DeclareApplet<EnigmaJr>{45, 0x02},
+    DeclareApplet<MarkoV>{98, 0x00},
+    DeclareApplet<MarkovPerc>{97, 0x00},
+    DeclareApplet<Pigeons>{71, 0x02},
+    DeclareApplet<TruthCat3>{94, 0x00},
+    DeclareApplet<TruthCat4>{93, 0x00},
+    DeclareApplet<PolyDiv>{72, 0x06},
+    DeclareApplet<ProbabilityDivider>{59, 0x04},
+    DeclareApplet<ProbabilityMelody>{62, 0x04},
+    DeclareApplet<ResetClock>{70, 0x14},
     DeclareApplet<Seq32>{75, 0x02},
     DeclareApplet<SeqPlay7>{76, 0x02},
     DeclareApplet<SequenceX>{14, 0x02},
-    DeclareApplet<ShiftGate>{48, 0x45},
-#ifdef PEWPEWPEW
     DeclareApplet<ShiftReg>{77, 0x45},
-#endif
     DeclareApplet<Shredder>{58, 0x01},
-    DeclareApplet<Shuffle>{36, 0x04},
-    DeclareApplet<Slew>{19, 0x01},
-    DeclareApplet<Squanch>{46, 0x08},
-    DeclareApplet<Stairs>{61, 0x01},
     DeclareApplet<Strum>{74, 0x08},
-    DeclareApplet<Switch>{3, 0x10},
     DeclareApplet<SwitchSeq>{38, 0x10},
-    DeclareApplet<TB_3PO>{60, 0x02},
-    DeclareApplet<TLNeuron>{13, 0x40},
-#ifdef PEWPEWPEW
-    DeclareApplet<Trending>{37, 0x40},
-#endif
     DeclareApplet<TrigSeq>{11, 0x06},
     DeclareApplet<TrigSeq16>{25, 0x06},
-    DeclareApplet<Tuner>{39, 0x80},
     DeclareApplet<TwoRings>{18, 0x02},
+    DeclareApplet<TB_3PO>{60, 0x02},
+#endif
+#ifdef APPLETS_QUANTIZERS
+    DeclareApplet<Chordinator>{64, 0x08},
+    DeclareApplet<Calibr8>{88, 0x10},
+    DeclareApplet<DualQuant>{9, 0x08},
+    DeclareApplet<DuoTET>{63, 0x08},
+    DeclareApplet<OffsetQuant>{90, 0x08},
+    DeclareApplet<EnsOscKey>{35, 0x08},
+    DeclareApplet<MultiScale>{73, 0x08},
+    DeclareApplet<ScaleDuet>{26, 0x08},
+    DeclareApplet<Squanch>{46, 0x08},
+#endif
+#ifdef APPLETS_MODULATION
+    DeclareApplet<ADSREG>{8, 0x01},
+    DeclareApplet<ADEG>{34, 0x01},
+    DeclareApplet<CVRecV2>{24, 0x02},
+    DeclareApplet<EbbAndLfo>{7, 0x01},
+    DeclareApplet<EnvSeq>{91, 0x02},
+    DeclareApplet<LowerRenz>{21, 0x01},
+    DeclareApplet<Relabi>{89, 0x01},
+    DeclareApplet<RndWalk>{69, 0x01},
+    DeclareApplet<Stairs>{61, 0x01},
     DeclareApplet<VectorEG>{52, 0x01},
     DeclareApplet<VectorLFO>{49, 0x01},
     //DeclareApplet<VectorMod>{53, 0x01}, // awkward middle child
     DeclareApplet<VectorMorph>{54, 0x01},
+#endif
+#ifdef APPLETS_LOGIC_UTILITY
+    DeclareApplet<AttenuateOffset>{56, 0x10},
+    DeclareApplet<Brancher>{4, 0x14},
+    DeclareApplet<Button>{65, 0x10},
+    DeclareApplet<Calculate>{12, 0x10},
+    DeclareApplet<Combin8>{82, 0x10},
+    DeclareApplet<Compare>{30, 0x10},
+    DeclareApplet<Cumulus>{5, 0x40},
+    DeclareApplet<EnvFollow>{42, 0x11},
+    DeclareApplet<GateDelay>{29, 0x04},
+    DeclareApplet<GatedVCA>{17, 0x50},
+    DeclareApplet<Logic>{10, 0x44},
+    DeclareApplet<Metronome>{50, 0x04},
+    DeclareApplet<hMIDIIn>{150, 0x20},
+    DeclareApplet<hMIDIOut>{27, 0x20},
+    DeclareApplet<Schmitt>{40, 0x40},
+    DeclareApplet<Scope>{23, 0x80},
+    DeclareApplet<ShiftGate>{48, 0x45},
+    DeclareApplet<Shuffle>{36, 0x04},
+    DeclareApplet<Slew>{19, 0x01},
+    DeclareApplet<Switch>{3, 0x10},
+    DeclareApplet<TLNeuron>{13, 0x40},
+    DeclareApplet<Trending>{37, 0x40},
+    DeclareApplet<Tuner>{39, 0x80},
     DeclareApplet<Voltage>{43, 0x10},
-#ifdef PEWPEWPEW
+    DeclareApplet<Xfader>{33, 0x10},
+#endif
+#ifdef APPLETS_WEIRDSTUFF
+    DeclareApplet<BitBeat>{79, 0x01},
+    DeclareApplet<BootsNCat>{55, 0x80},
+    DeclareApplet<BugCrack>{51, 0x80},
+    DeclareApplet<Binary>{41, 0x41},
+    DeclareApplet<DrLoFi>{16, 0x80},
+    DeclareApplet<Palimpsest>{20, 0x02},
+    DeclareApplet<Ponglet>{99, 0x00},
+    DeclareApplet<RunglBook>{44, 0x01},
+#endif
+#ifdef APPLETS_CRAZYTOWN
+    DeclareApplet<MidiLoop>{81, 0x20},
+    DeclareApplet<GameOfLife>{22, 0x01},
     DeclareApplet<WTVCO>{67, 0x80},
 #endif
-    DeclareApplet<Xfader>{33, 0x10},
+
+#endif // !PEWPEWPEW
+
 };
 
 
