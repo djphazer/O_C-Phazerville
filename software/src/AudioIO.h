488598c5a82a779fde10499b4733f4f66a9bf88e
Nicholas J. Michalek <djphazer@gmail.com>
2026-06-08 22:16:05 -0400

    AudioMemory: 384 blocks for T41_audio build
    For some reason, changing this causes audio glitches to appear/disappear

--- software/src/AudioIO.h
+++ software/src/AudioIO.h
@@ -4,7 +4,11 @@
 
 namespace OC {
   namespace AudioIO {
+#ifdef USB_AUDIO
+    const int AUDIO_MEMORY = 384;
+#else
     const int AUDIO_MEMORY = 512;
+#endif
     AudioStream& InputStream(int interface = 0);
     AudioStream& OutputStream();
     void Init();
